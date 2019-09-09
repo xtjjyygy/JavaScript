6bb7ef5c9f366599014276ebfca5114993294e4b
xtjjyygy <38133593+xtjjyygy@users.noreply.github.com>
2019-09-09 08:39:17 +0800

    Create throttle.js

---
+++ throttle.js
@@ -0,0 +1,16 @@
+var throttle = function(func, delay) {            
+　　var prev = Date.now();            
+　　return function() {                
+　　　　var context = this;                
+　　　　var args = arguments;                
+　　　　var now = Date.now();                
+　　　　if (now - prev >= delay) {                    
+　　　　　　func.apply(context, args);                    
+　　　　　　prev = Date.now();                
+　　　　}            
+　　}        
+}        
+function handle() {            
+　　console.log(Math.random());        
+}        
+window.addEventListener('scroll', throttle(handle, 1000));
