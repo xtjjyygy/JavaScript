d359691ce8ea8db9fb432ea8077c10e451095058
xtjjyygy <38133593+xtjjyygy@users.noreply.github.com>
2019-09-18 09:08:55 +0800

    Update curry.js

--- curry.js
+++ curry.js
@@ -19,3 +19,25 @@ function add () {
   console.log(add(1)(2)) // 3
   console.log(add(1)(2)(3)(4)) // 6
   console.log(add(1,2,3)(4)) // 10
+
+
+#方法二:通用方法
+
+function add(...args1) {
+    // 第一次执行时，定义一个数组专门用来存储所有的参数
+    var _args = [...args1];
+    // 在内部声明一个函数，利用闭包的特性保存_args并收集所有的参数值
+    var _adder = function () {
+        _args.push(...arguments);
+        return _adder;
+    };
+ 
+    // 利用toString隐式转换的特性，当最后执行时隐式转换，并计算最终的值返回
+    _adder.toString = function () {
+        return _args.reduce( (a, b) => a + b);
+    }
+    return _adder;
+}
+
+result = add(1,2,3)(4,5,6) 
+console.log(result)
