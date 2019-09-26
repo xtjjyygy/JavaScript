30914775182f50325beb805928e69836b9b96740
xtjjyygy <38133593+xtjjyygy@users.noreply.github.com>
2019-09-26 09:49:00 +0800

    Create copy.js

---
+++ copy.js
@@ -0,0 +1,46 @@
+//deepCopy
+
+1，循环+递归
+
+function deepClone(obj){
+    let objClone = Array.isArray(obj)?[]:{};
+    if(obj && typeof obj==="object"){
+        for(key in obj){
+            if(obj.hasOwnProperty(key)){
+                //判断ojb子元素是否为对象，如果是，递归复制
+                if(obj[key]&&typeof obj[key] ==="object"){
+                    objClone[key] = deepClone(obj[key]);
+                }else{
+                    //如果不是，简单复制
+                    objClone[key] = obj[key];
+                }
+            }
+        }
+    }
+    return objClone;
+}    
+let a=[1,2,3,4],
+    b=deepClone(a);
+a[0]=2;
+console.log(a,b);
+
+//2，JSON
+//JSON.parse(JSON.stringfiy(obj))
+function deepClone(obj){
+    let _obj = JSON.stringify(obj),
+        objClone = JSON.parse(_obj);
+    return objClone
+}    
+let a=[0,1,[2,3],4],
+    b=deepClone(a);
+a[0]=1;
+a[2][0]=1;
+console.log(a,b);
+
+//3,jquery中的extend
+
+let a=[0,1,[2,3],4],
+    b=$.extend(true,[],a);
+a[0]=1;
+a[2][0]=1;
+console.log(a,b);
